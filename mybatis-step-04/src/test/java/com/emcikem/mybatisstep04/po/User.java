20c0fc4473be513997c33de372982e2f85d180ce
Emcikem <18968479985@163.com>
2023-01-22 20:02:43 +0800

    delete(yml, do):删除其他xml

--- mybatis-step-04/src/test/java/com/emcikem/mybatisstep04/po/User.java
+++ mybatis-step-04/src/test/java/com/emcikem/mybatisstep04/po/User.java
@@ -10,7 +10,7 @@ import java.util.Date;
 public class User {
 
     private Long id;
-    private String userId;          // 用户ID
+    private Long userId;          // 用户ID
     private String userHead;        // 头像
     private Date createTime;        // 创建时间
     private Date updateTime;        // 更新时间
@@ -24,11 +24,11 @@ public class User {
         this.id = id;
     }
 
-    public String getUserId() {
+    public Long getUserId() {
         return userId;
     }
 
-    public void setUserId(String userId) {
+    public void setUserId(Long userId) {
         this.userId = userId;
     }
 
