2e2162ca202f0ae4bea9060a6b1561d51fbff786
Emcikem <18968479985@163.com>
2023-01-24 18:18:00 +0800

    feat(service): 无池化数据库连接池

---
+++ mybatis-step-04/src/main/java/com/emcikem/mybatisstep04/datasource/pooled/PooledDataSource.java
@@ -0,0 +1,11 @@
+package com.emcikem.mybatisstep04.datasource.pooled;
+
+import javax.sql.DataSource;
+
+/**
+ * @author Emcikem
+ * @create 2023/1/24
+ * @desc 有连接池的数据源
+ */
+public class PooledDataSource implements DataSource {
+}
---
+++ mybatis-step-04/src/main/java/com/emcikem/mybatisstep04/datasource/pooled/PooledDataSourceFactory.java
@@ -0,0 +1,9 @@
+package com.emcikem.mybatisstep04.datasource.pooled;
+
+/**
+ * @author Emcikem
+ * @create 2023/1/24
+ * @desc
+ */
+public class PooledDataSourceFactory {
+}
